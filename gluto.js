4069caa7735be786c7ec413fd3e6513467b21b60
Michael Klich <michal@michalklich.com>
2016-01-12 23:36:26 +0000

    Add basic package, and first code.
    Most is based on example from botkit.
    Add notes on features to come and function to randomly pick a place to eat.

---
+++ gluto.js
@@ -0,0 +1,147 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Gluto
+
+This is a sample Slack bot built with Botkit.
+
+- TODO: Voting on place to eat, each user can cast one vote where he would like to
+eat
+- TODO: Return a list of all added places
+- TODO: Provide details of a place from the list.
+        * link to menu
+        * most popular meals
+        * address
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('botkit');
+var os = require('os');
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+
+// TODO: come up with some better names
+controller.hears(['random', 'food'], 'direct_message,direct_mention,mention',function(bot, message) {
+
+    var places = ['El Burrito', 'The Cafe', 'Papaya', 'Tesco', 'Wasabi', 'Byron',
+                  'Pizza Express', 'Wahaca'];
+    bot.reply(message, "I'd recommend " + places[Math.floor(Math.random() * places.length)]);
+
+});
+
+
+controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    },function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(',err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user,function(err, user) {
+        if (user && user.name) {
+            bot.reply(message,'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message,'Hello.');
+        }
+    });
+});
+
+controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
+    var matches = message.text.match(/call me (.*)/i);
+    var name = matches[1];
+    controller.storage.users.get(message.user,function(err, user) {
+        if (!user) {
+            user = {
+                id: message.user,
+            };
+        }
+        user.name = name;
+        controller.storage.users.save(user,function(err, id) {
+            bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
+        });
+    });
+});
+
+
+controller.hears(['what is my name','who am i'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    controller.storage.users.get(message.user,function(err, user) {
+        if (user && user.name) {
+            bot.reply(message,'Your name is ' + user.name);
+        } else {
+            bot.reply(message,'I don\'t know yet!');
+        }
+    });
+});
+
+
+controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    bot.startConversation(message,function(err, convo) {
+        convo.ask('Are you sure you want me to shutdown?',[
+            {
+                pattern: bot.utterances.yes,
+                callback: function(response, convo) {
+                    convo.say('Bye!');
+                    convo.next();
+                    setTimeout(function() {
+                        process.exit();
+                    },3000);
+                }
+            },
+        {
+            pattern: bot.utterances.no,
+            default: true,
+            callback: function(response, convo) {
+                convo.say('*Phew!*');
+                convo.next();
+            }
+        }
+        ]);
+    });
+});
+
+
+controller.hears(['uptime','identify yourself','who are you','what is your name'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    var hostname = os.hostname();
+    var uptime = formatUptime(process.uptime());
+
+    bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+
+});
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
